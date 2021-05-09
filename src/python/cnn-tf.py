a4501829be80298da86a717907cbcf1fd208bd3d
zikunxu <zx272@cornell.edu>
2021-05-09 05:16:19 -0400

    🌈 style(cnn-tf): Rename + Comment

--- src/python/cnn-tf.py
+++ src/python/cnn-tf.py
@@ -1,5 +1,5 @@
-# Tensorflow CPU 
-# run with python cnn_cpu.py 
+# Tensorflow (can run with tensorflow or tensorflow-gpu) 
+# run with python cnn-tf.py 
 
 import tensorflow as tf 
 from tensorflow.keras.datasets import mnist 
@@ -7,15 +7,12 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras import layers 
 import time 
 
-
-
-# if __name__ = "__main__":
-
-(x_train,y_train),(x_test,y_test)=mnist.load_data()  #可以直接用API加载MNIST的数据
+# load data
+(x_train,y_train),(x_test,y_test)=mnist.load_data()  # MNIST data can be loaded through this API
 x_train = x_train.reshape((60000,28,28,1))
 x_test = x_test.reshape((10000,28,28,1))
 
-
+# build model
 model = Sequential()
 model.add(layers.Conv2D(filters=6, kernel_size=(5,5),input_shape=(28,28,1)))
 model.add(layers.MaxPooling2D( pool_size = (4,4)))
@@ -24,15 +21,15 @@ model.add(layers.Dense(10,activation='softmax'))
 
 model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
 
-model.summary() # print summary of model structure
+# print summary of model structure
+model.summary() 
 
 tic = time.perf_counter() # timing
 model.fit(x_train, y_train, epochs=5, batch_size = 100) 
 toc = time.perf_counter()
 
+# evaluate this model with test data
 val_loss,val_acc=model.evaluate(x_test,y_test) 
 
-print("Time cost = {:.3f}".format(toc-tic))
+print("Time cost = {:.3f}".format(toc-tic)) # print the training time
 print("Test loss = {}. accuracy = {}".format(val_loss, val_acc))
-
-
